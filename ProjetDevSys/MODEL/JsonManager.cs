160d9d5a5ba21cbdff9c024f4e603da61abc8ebd
alexandrethurel <88685951+alexandrethurel@users.noreply.github.com>
2024-02-07 17:36:16 +0100

    Ajout Json Manager

---
+++ ProjetDevSys/MODEL/JsonManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.Json;
+using System.IO;
+
+namespace ProjetDevSys.MODEL
+{
+    public class JsonManager
+    {
+        public string JsonPath { get; set; }
+
+        public JsonManager(string jsonPath)
+        {
+            JsonPath = jsonPath;
+        }
+
+        public void Serialize<T>(T obj)
+        {
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            string json = JsonSerializer.Serialize(obj, options);
+            File.WriteAllText(JsonPath, json);
+        }
+
+        public T Deserialize<T>()
+        {
+            if (!File.Exists(JsonPath))
+            {
+                throw new FileNotFoundException($"Le fichier {JsonPath} n'existe pas.");
+            }
+
+            string json = File.ReadAllText(JsonPath);
+            return JsonSerializer.Deserialize<T>(json);
+        }
+    }
+}
