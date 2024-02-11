5f1ae3a60240b15fe7281d658f9b8656e14197f1
alexandrethurel <88685951+alexandrethurel@users.noreply.github.com>
2024-02-11 14:53:51 +0100

    Fixed problem color in RunTaskView file

--- ProjetDevSys/Vue/RunTaskView.cs
+++ ProjetDevSys/Vue/RunTaskView.cs
@@ -46,14 +46,11 @@ namespace ProjetDevSys.Vue
                     bool result = runSaveTask.RunTask(inputId);
                     if (result)
                     {
-                        Console.ForegroundColor = ConsoleColor.Green;
                         return ResourceHelper.GetString("RunTaskView3");
                     }
                     else
                     {
-                        Console.ForegroundColor = ConsoleColor.Red;
                         return ResourceHelper.GetString("RunTaskView4");
-
                     }
                 }
                 else
@@ -105,7 +102,6 @@ namespace ProjetDevSys.Vue
                     // Check if the start and end ID are valid
                     if (startId < 0 || endId >= BackupList.Count())
                     {
-                        Console.ForegroundColor = ConsoleColor.Red;
                         return ResourceHelper.GetString("RunTaskView19");
                     }
 
@@ -115,12 +111,10 @@ namespace ProjetDevSys.Vue
 
                     if (result)
                     {
-                        Console.ForegroundColor = ConsoleColor.Green;
                         return ResourceHelper.GetString("RunTaskView6");
                     }
                     else
                     {
-                        Console.ForegroundColor = ConsoleColor.Red;
                         return ResourceHelper.GetString("RunTaskView7");
                     }
                 }
