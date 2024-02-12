c956aa246fa48791cbcc0372c02b47d0d9d69811
Paul Bréon <paul.breon@viacesi.fr>
2024-02-12 13:57:54 +0100

    Link between view and viewModel

--- ProjetDevSysGraphical/MainWindow.xaml.cs
+++ ProjetDevSysGraphical/MainWindow.xaml.cs
@@ -17,24 +17,11 @@ namespace ProjetDevSysGraphical
     /// </summary>
     public partial class MainWindow : Window
     {
+        private GestionTask gestionTask = new GestionTask();
+
         public MainWindow()
         {
             InitializeComponent();
-        }
-        
-        public void ButtonAdd_Click(object sender, RoutedEventArgs e)
-        {
-            
-        }
-        
-        public void ButtonlaunchAllTasks_Click(object sender, RoutedEventArgs e)
-        {
-            
-        }
-        
-        public void ButtonlaunchSelectedTasks_Click(object sender, RoutedEventArgs e)
-        {
-            
         }
 
         public void ButtonInstance()
@@ -44,13 +31,39 @@ namespace ProjetDevSysGraphical
                 // Create a button for each row in the DataGrid
                 Button button = new Button();
                 button.Content = "Delete";
+                button.Click += new RoutedEventHandler(ButtonDelete_Click);
 
                 // Create a click event for each row in the DataGrid
                 CheckBox checkBox = new CheckBox();
                 checkBox.HorizontalAlignment = HorizontalAlignment.Left;
                 checkBox.VerticalAlignment = VerticalAlignment.Bottom;
-                // checkBox.Margin = Datagrid.row.position;
+                checkBox.Margin = new Thickness(635, 0, 0, 305);
             }
+        }
+
+        public void ButtonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            // Add a new task
+            // gestionTask.CreateTask("fileName", "sourcePath", "destinationPath", "backupType");
+        }
+
+        public void ButtonlaunchAllTasks_Click(object sender, RoutedEventArgs e)
+        {
+            // Launch all tasks
+            foreach (ItemCollection item in dataGrid.Items)
+            {
+                // gestionTask.CreateTask("fileName", "sourcePath", "destinationPath", "backupType");
+            }
+        }
+
+        public void ButtonlaunchSelectedTasks_Click(object sender, RoutedEventArgs e)
+        {
+            // Launch selected tasks
+        }
+
+        private void ButtonDelete_Click(object sender, RoutedEventArgs e)
+        {
+            gestionTask.DeleteTask(dataGrid.SelectedIndex);
         }
     }
 }
