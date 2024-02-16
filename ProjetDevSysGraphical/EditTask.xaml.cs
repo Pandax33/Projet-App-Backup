f0c570c3a6c457fccae62b8619b26f9a6bfcb4b0
Léandro DE BARROS BARBOSA <leandro.debarrosbarbosa@viacesi.fr>
2024-02-16 10:27:41 +0100

    add user control

--- ProjetDevSysGraphical/EditTask.xaml.cs
+++ ProjetDevSysGraphical/EditTask.xaml.cs
@@ -81,7 +81,11 @@ namespace ProjetDevSysGraphical
                     gestionTask.EditTask(Id, FileTarget, FileSource, SaveType);
 
                     MainWindow mainWindow = Application.Current.MainWindow as MainWindow;
-                    mainWindow.GenerateGrid();
+                    if (mainWindow != null && mainWindow.contentControl.Content is Backup backup)
+                    {
+                        // Appeler GenerateGrid sur cette instance
+                        backup.GenerateGrid();
+                    }
                 }
                 if (gestionTask.VerifSource(FileSource) == false && gestionTask.VerifSource(FileTarget) == true)
                 {
--- ProjetDevSysGraphical/MainWindow.xaml.cs
+++ ProjetDevSysGraphical/MainWindow.xaml.cs
@@ -23,168 +23,29 @@ namespace ProjetDevSysGraphical
         private List<Button> editButtonList = new List<Button>();
         private List<string> buttonNameList = new List<string>();
         private List<int> idToLaunch = new List<int>();
+        private Backup Backup;
 
         public MainWindow()
         {
             InitializeComponent();
-            GenerateGrid();
-            ButtonInstance(stackPanel);
+            Backup = new Backup();
+            
         }
 
-        public void GenerateGrid()
+        
+
+        private void Home_Click(object sender, RoutedEventArgs e)
         {
-            listView.Items.Clear();
-            BackupGridViewModel backupGridViewModel = new BackupGridViewModel();
-            var backups = backupGridViewModel.GetAllBackupsModel();
-            foreach(var backup in backups) // backup type = Backup
-            {
-                listView.Items.Add(new
-                {
-                    Propriete1 = backup.Name,
-                    Propriete2 = backup.Source,
-                    Propriete3 = backup.Destination,
-                    Propriete4 = backup.Type
-                });
-                SetButtonName(backup.Name);
-            }
-            ButtonInstance(stackPanel);
+            contentControl.Content = new Accueil();
         }
 
-        public void ButtonInstance(StackPanel container)
+        private void Backup_Click(object sender, RoutedEventArgs e)
         {
-            container.Children.Clear();
-            int[] index = new int[listView.Items.Count];
-            for (int i = 0; i < listView.Items.Count; i++)
-            {
-                index[i] = i;
+            contentControl.Content = Backup;
 
-                // Create a horizontal StackPanel for each row in the DataGrid
-                StackPanel panel = new StackPanel();
-                panel.Orientation = Orientation.Horizontal;
-
-                // Create a button for Delete
-                Button buttonDelete = new Button();
-                buttonDelete.Width = 30;
-                buttonDelete.Height = 30;
-                buttonDelete.Margin = new Thickness(5, 0, 5, 0);
-                buttonDelete.Background = Brushes.Red;
-                buttonDelete.Click += new RoutedEventHandler(ButtonDelete_Click);
-
-                // Create a button for Edit
-                Button buttonEdit = new Button();
-                buttonEdit.Width = 30;
-                buttonEdit.Height = 30;
-                buttonEdit.Margin = new Thickness(5, 0, 5, 0);
-                buttonEdit.Background = Brushes.Blue;
-                buttonEdit.Click += new RoutedEventHandler(ButtonEdit_Click);
-
-                // Create a checkbox
-                CheckBox checkBox = new CheckBox();
-                checkBox.Width = 30;
-                checkBox.Height = 30;
-                checkBox.Margin = new Thickness(5, 0, 5, 0);
-                checkBox.Click += new RoutedEventHandler(CheckBox_Click);
-
-                // Add the buttons to the StackPanel
-                panel.Children.Add(buttonDelete);
-                panel.Children.Add(buttonEdit);
-                panel.Children.Add(checkBox);
-
-                // Add the StackPanel to the container
-                container.Children.Add(panel);
-
-                buttonDelete.Name = buttonNameList[i] + "_" + index[i].ToString() + "_" + "Delete";
-                buttonEdit.Name = buttonNameList[i] + "_" + index[i].ToString() + "_" + "Edit";
-                checkBox.Name = buttonNameList[i] + "_" + index[i].ToString() + "_" + "CheckBox";
-                // Add the buttons to the list
-                deleteButtonList.Add(buttonDelete);
-                editButtonList.Add(buttonEdit);
-            }
         }
 
-        private void SetButtonName(string buttonName)
-        {
-            buttonNameList.Add(buttonName);
-        }
-
-        #region ButtonClicks
-        private void CheckBox_Click(object sender, RoutedEventArgs e)
-        {
-            CheckBox clicked = sender as CheckBox;
-            if (clicked != null)
-            {
-                // Extract the index from the button's name
-                int index = int.Parse(clicked.Name.Split('_')[1]);
-
-                if (clicked.IsChecked == true)
-                {
-                    // Add the index to the list if it is checked
-                    idToLaunch.Add(index);
-                }
-                else
-                {
-                    // Delete the index from the list if it is unchecked
-                    idToLaunch.Remove(index);
-                }
-            }
-        }
-
-
-        public void ButtonAdd_Click(object sender, RoutedEventArgs e)
-        {
-            // Add a new task
-            AddTask addTask = new AddTask();
-            addTask.ShowDialog();
-        }
-
-        public void ButtonlaunchAllTasks_Click(object sender, RoutedEventArgs e)
-        {
-            RunSaveTask runSaveTask = new RunSaveTask();
-            // Launch all tasks
-            runSaveTask.RunMultipleTask(0, listView.Items.Count);
-        }
-
-        public void ButtonlaunchSelectedTasks_Click(object sender, RoutedEventArgs e)
-        {
-            RunSaveTask runSaveTask = new RunSaveTask();
-            runSaveTask.RunTaskMultiple(idToLaunch.ToArray());
-        }
-
-        private void ButtonDelete_Click(object sender, RoutedEventArgs e)
-        {
-            Button clickedButton = sender as Button;
-            if (clickedButton != null)
-            {
-                // Extract the index from the button's name
-                string buttonName = clickedButton.Name;
-                buttonName = buttonName.Split('_')[0];
-
-                int index = int.Parse(clickedButton.Name.Split('_')[1]);
-
-                // Call DeleteTask with the extracted index
-                gestionTask.DeleteTask(index);
-                PopUpWPF popUpWPF = new PopUpWPF(buttonName + " " + "Task deleted successfully");
-                popUpWPF.ShowDialog();
-            }
-        }
-
-        private void ButtonEdit_Click(object sender, RoutedEventArgs e)
-        {
-            Button clickedButton = sender as Button;
-            if (clickedButton != null)
-            {
-                // Extract the index from the button's name
-                string buttonName = clickedButton.Name;
-                buttonName = buttonName.Split('_')[0];
-
-                int index = int.Parse(clickedButton.Name.Split('_')[1]);
-
-                // Call EditTask view
-                EditTask editTask = new EditTask(index, buttonName, null, null, null);
-                editTask.ShowDialog();
-            }
-        }
-        #endregion
+       
 
         private void MainWindow_Closed(object sender, System.ComponentModel.CancelEventArgs e)
         {
@@ -193,7 +54,7 @@ namespace ProjetDevSysGraphical
 
         private void Settings_Click(object sender, RoutedEventArgs e)
         {
-            new Config().ShowDialog();
+            contentControl.Content = new ConfigControl();
         }
     }
 }
