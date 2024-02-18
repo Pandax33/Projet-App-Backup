85faf9fa2e00679cb4270796e9ded3e8df474170
Léandro DE BARROS BARBOSA <leandro.debarrosbarbosa@viacesi.fr>
2024-02-18 23:27:59 +0100

    fix edit

--- ProjetDevSysGraphical/EditTask.xaml.cs
+++ ProjetDevSysGraphical/EditTask.xaml.cs
@@ -58,7 +58,7 @@ namespace ProjetDevSysGraphical
             {
                 if (ProjetDevSys.AppConstants.VerifExist(source) == true && ProjetDevSys.AppConstants.VerifExist(target) == true)
                 {
-                    MessageBox.Show(gestionTask.EditTask(Id, source, target, type), ResourceHelper.GetString("Task.Popup.Out"), MessageBoxButton.OK, MessageBoxImage.Information);
+                    MessageBox.Show(gestionTask.EditTask(Id, target,source, type), ResourceHelper.GetString("Task.Popup.Out"), MessageBoxButton.OK, MessageBoxImage.Information);
 
                     MainWindow mainWindow = Application.Current.MainWindow as MainWindow;
                     // Appeler GenerateGrid sur cette instance
