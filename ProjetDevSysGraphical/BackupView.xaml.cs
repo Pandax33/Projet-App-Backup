38238430d0d2bb6855154cb48e74a29626d0cb8b
Léandro DE BARROS BARBOSA <leandro.debarrosbarbosa@viacesi.fr>
2024-02-27 21:11:51 +0100

    new liste

--- ProjetDevSysGraphical/BackupView.xaml.cs
+++ ProjetDevSysGraphical/BackupView.xaml.cs
@@ -375,31 +375,33 @@ namespace ProjetDevSysGraphical
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
+            }            
         }
 
 
         public async void selectedTasksButton_Click(object sender, RoutedEventArgs e)
         {
-            MainWindow mainWindow = Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
-            mainWindow.contentControl.Content = new Accueil();
-
-            RunTaskAsync runSaveTask = new RunTaskAsync();
-            // Obtenez le contexte de synchronisation actuel pour l'UI thread
+    
             SynchronizationContext context = SynchronizationContext.Current;
+            BackupManager.SetSynchronizationContext(context);
+            int[] idList = idToLaunch.ToArray();
 
             try
             {
-                // Await l'opération asynchrone et stockez le résultat
-                string result = await runSaveTask.RunTaskMultipleAsync(idToLaunch.ToArray(), context);
-                // Affichez le résultat dans une MessageBox après la fin des tâches
-                MessageBox.Show(result, "Run", MessageBoxButton.OK, MessageBoxImage.Information);
+                BackupManager.AddBackupToQueue(idList);
+
             }
             catch (Exception ex)
             {
-                // Gérez ou loggez l'exception si nécessaire
-                MessageBox.Show("An error occurred: " + ex.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                // En cas d'erreur, affichez un message
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
+            MainWindow mainWindow = Application.Current.Windows.OfType<MainWindow>().FirstOrDefault();
+            if (mainWindow != null)
+            {
+                mainWindow.contentControl.Content = new Accueil();
+            }
+
         }
 
         private void ButtonDelete_Click(object sender, RoutedEventArgs e)
---
+++ ProjetDevSysGraphical/VueModel/BackupManager.cs
@@ -0,0 +1,87 @@
+﻿using ProjetDevSys.Model;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ProjetDevSysGraphical.VueModel
+{
+    public static class BackupManager
+    {
+        private static List<BackupJob> backupQueue = new List<BackupJob>();
+        private static SynchronizationContext context = SynchronizationContext.Current;
+
+        public static void SetSynchronizationContext(SynchronizationContext newContext)
+        {
+            context = newContext;
+        }
+
+        public static void AddBackupToQueue(int[] backupIds)
+        {
+            foreach (int id in backupIds)
+            {
+                Backup backup = BackupFactory.GetBackupByIndex(id);
+                if (backup != null && !ProjetDevSys.AppConstants.backupState.ContainsKey(backup.Name))
+                {
+                    BackupJob backupJob = new BackupJob(backup);
+                    backupQueue.Add(backupJob);
+                    ProjetDevSys.AppConstants.backupProgress.TryAdd(backup.Name, backupJob.LogRealTime.Progress);
+                    ManualResetEvent mre = new ManualResetEvent(true);
+                    ProjetDevSys.AppConstants.BackupPauseHandles.TryAdd(backup.Name, mre);
+                    var cts = new CancellationTokenSource();
+                    ProjetDevSys.AppConstants.BackupCancellations[backup.Name] = cts;
+                }
+            }
+            foreach (var backupProgress in ProjetDevSys.AppConstants.backupProgress)
+            {
+                Debug.WriteLine($"Backup: {backupProgress.Key}, Progress: {backupProgress.Value}");
+            }
+            ExecuteBackups(); 
+        }
+
+        private static async void ExecuteBackups()
+        {
+            foreach (BackupJob backupJob in backupQueue.ToList())
+            {
+                if (!ProjetDevSys.AppConstants.backupState.ContainsKey(backupJob.Backup.Name))
+                {
+                    ProjetDevSys.AppConstants.backupState.TryAdd(backupJob.Backup.Name, "In Progress");
+                    await ExecuteBackupAsync(backupJob);
+                }
+            }
+
+            if (ProjetDevSys.AppConstants.backupState.IsEmpty)
+            {
+                context.Post(_ => MessageBox.Show("All backups completed successfully.", "Backup Complete", MessageBoxButton.OK, MessageBoxImage.Information), null);
+            }
+        }
+
+        private static Task ExecuteBackupAsync(BackupJob backupJob)
+        {
+            return Task.Run(() =>
+            {
+                try
+                {
+                    backupJob.CreateLogRealTime();
+                    ProjetDevSys.AppConstants.priorityEvent.Reset();
+                    backupJob.SavePrio();
+                    ProjetDevSys.AppConstants.priorityEvent.Set();
+                    backupJob.Save();
+                    string value;
+                    ProjetDevSys.AppConstants.backupState.TryRemove(backupJob.Backup.Name, out value);
+                    ProjetDevSys.AppConstants.BackupCancellations.TryRemove(backupJob.Backup.Name, out _);
+                    ProjetDevSys.AppConstants.BackupPauseHandles.TryRemove(backupJob.Backup.Name, out _);
+                    ProjetDevSys.AppConstants.backupProgress.TryRemove(backupJob.Backup.Name, out _);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error during backup for {backupJob.Backup.Name}: {ex}");
+                }
+            });
+        }
+    }
+}
